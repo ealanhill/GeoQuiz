d5fc2f7e423eec1ec81f4033728dfe6ccc0e1d2b
Alan Hill <alan.hill@willowtreeapps.com>
2016-08-17 08:53:16 -0400

    Images & Questions
    Add new images for the next and previous features. Add new questions and the ability
    to randomly choose a question on next.

---
+++ app/src/main/java/com/bignerdranch/ahill/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.ahill.geoquiz;
+
+/**
+ * Created by alanhill on 8/15/16.
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        this.mAnswerTrue = answerTrue;
+    }
+}
